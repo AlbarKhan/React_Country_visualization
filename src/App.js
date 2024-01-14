cc821e6e5172c8af05fcf9564a3de41dfa112278
unknown <albarkhan60@gmail.com>
2024-01-14 19:58:33 +0530

    Hero section done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import "./App.css";
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Hero />
     </div>
   );
 }
 
-export default App;
+function Hero() {
+  return (
+    <div className="hero">
+      <h1>Country visualisation</h1>
+      {/* <img src="globe-2.jpg"></img> */}
+    </div>
+  );
+}
